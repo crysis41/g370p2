10c5af9789330c597754a9b16e8531ccd5f5ac78
crysis41 <42611634+crysis41@users.noreply.github.com>
2020-04-13 23:44:48 -0500

    Update nameGenerator.cs

--- g370p3/Assets/Scripts/nameGenerator.cs
+++ g370p3/Assets/Scripts/nameGenerator.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class nameGenerator : MonoBehaviour
 {
+    public string charName;
+
+    string[] firstName;
+    string[] lastName;
+
     // Start is called before the first frame update
     void Start()
     {
