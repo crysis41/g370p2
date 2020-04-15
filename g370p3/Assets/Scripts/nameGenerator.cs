68e6c4c141e991944da7c5086fa6902d2bbc0dfa
crysis41 <42611634+crysis41@users.noreply.github.com>
2020-04-14 19:28:45 -0500

    Finished program

--- g370p3/Assets/Scripts/nameGenerator.cs
+++ g370p3/Assets/Scripts/nameGenerator.cs
@@ -9,10 +9,18 @@ public class nameGenerator : MonoBehaviour
     string[] firstName;
     string[] lastName;
 
+    int randFirst, randLast;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstName = System.IO.File.ReadAllLines(@"C:\Users\wilso\OneDrive\Documents\GitHub\g370p2\g370p3\Assets\firstNames.txt");
+        lastName = System.IO.File.ReadAllLines(@"C:\Users\wilso\OneDrive\Documents\GitHub\g370p2\g370p3\Assets\lastNames.txt");
+
+        randFirst = Random.Range(0, 79);
+        randLast = Random.Range(0, 79);
+
+        charName = firstName[randFirst] + " " + lastName[randLast] + "\n";
     }
 
     // Update is called once per frame
